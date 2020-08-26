fde06be157cf25a0a04f1411575fb198a9905ace
Tim Schommer <raqua@Tims-Laptop>
2020-08-26 08:16:17 -0500

    After more experimentation, a slightly more concise version

--- Backend/MusicBackend/src/main/java/com/helloworld/HelloWorldController.java
+++ Backend/MusicBackend/src/main/java/com/helloworld/HelloWorldController.java
@@ -4,13 +4,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class HelloWorldController 
 {
 	@RequestMapping("/hello")
-	@ResponseBody
 	public String sayHello()
 	{
 		return "Hello from Spring Boot";
