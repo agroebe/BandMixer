400b10b016af7e08bcbc28c6c77a207fc9dd7343
Aidan <aidanwebster987@gmail.com>
2020-08-30 17:55:07 -0500

    MySQL experiment project files added

---
+++ Experiments/MySQLExpriment/src/main/java/com/example/MySQLExpriment/MySqlExprimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySQLExpriment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlExprimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlExprimentApplication.class, args);
+	}
+
+}
