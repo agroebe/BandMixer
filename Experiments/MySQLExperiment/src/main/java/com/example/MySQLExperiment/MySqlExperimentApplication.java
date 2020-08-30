eecb822e60b77cc9597ebea633a4b557fff0ec6d
Aidan <aidanwebster987@gmail.com>
2020-08-30 18:43:39 -0500

    MySQL Experiment base project files

---
+++ Experiments/MySQLExperiment/src/main/java/com/example/MySQLExperiment/MySqlExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySQLExperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlExperimentApplication.class, args);
+	}
+
+}
---
+++ Experiments/MySQLExperiment/src/test/java/com/example/MySQLExperiment/MySqlExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySQLExperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
