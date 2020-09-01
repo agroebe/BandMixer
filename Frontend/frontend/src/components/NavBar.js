a6f6526d959466679d1559ccfbee239238009a17
Andrew Sandor <ajsandor@iastate.edu>
2020-09-01 13:44:29 -0500

    demo 1 minutes

--- Frontend/frontend/src/components/NavBar.js
+++ Frontend/frontend/src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar, Button } from 'react-bootstrap';
 import SignInModal from './modals/SignInModal'
 import RegisterModal from './modals/RegisterModal'
+import ProfileModal from './modals/ProfileModal'
 
 export default class NavBar extends React.Component {
     constructor(props) {
@@ -9,6 +10,7 @@ export default class NavBar extends React.Component {
 
         this.openSignInModal = this.openSignInModal.bind(this);
         this.openRegisterModal = this.openRegisterModal.bind(this);
+        this.openProfileModal = this.openProfileModal.bind(this);
     }
 
     /* This function will be passed in as a prop so it can be opened from the RegisterModal. */
@@ -23,6 +25,11 @@ export default class NavBar extends React.Component {
         this.registerModal.open()
     }
 
+    openProfileModal() {
+        //this.signInModal.close();
+        this.profileModal.open()
+    }
+
     render() {
         return(
             <Navbar bg="dark" variant="dark">
@@ -30,11 +37,13 @@ export default class NavBar extends React.Component {
                 <Navbar.Collapse className="justify-content-end">
                     <Button variant="primary" className="mr-2" onClick={ this.openSignInModal }>Sign In</Button>
                     <Button variant="secondary" onClick={ this.openRegisterModal }>Register</Button>
+                    <Button variant="primary" onClick={ this.openProfileModal }>Profile</Button>
                 </Navbar.Collapse>
 
                 { /* Modals for registering and signing in. */ }
                 <RegisterModal ref={ (modal) => { this.registerModal = modal } } openSignInModal={ this.openSignInModal }/>
                 <SignInModal ref={ (modal) => { this.signInModal = modal } } openRegisterModal={ this.openRegisterModal }/>
+                <ProfileModal ref={ (modal) => { this.profileModal = modal } } openProfileModal={ this.openProfileModal }/>
 
             </Navbar>
         )
