854d803fd6dd97957745a1957732cb3d44ac65a7
Aidan <aidanwebster987@gmail.com>
2020-09-01 12:21:10 -0500

    Connected MySQL database to spring boot web service

--- Experiments/MySQLExperiment/src/main/java/com/example/MySQLExperiment/UserRepository.java
+++ Experiments/MySQLExperiment/src/main/java/com/example/MySQLExperiment/UserRepository.java
@@ -1,2 +1,9 @@
-package com.example.MySQLExperiment;public class UserRepository {
+package com.example.MySQLExperiment;
+
+//This will be auto implemented by Spring into a bean called userRepository
+//CRUD means create, read, update, delete
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Integer> {
 }
