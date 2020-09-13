c8b4c2fa4d90e6469ff76050951b0da03f5fa4d3
Aidan <aidanwebster987@gmail.com>
2020-09-13 14:32:34 -0500

    Added checking if users are already in the database

--- Backend/MusicBackend/src/main/java/com/application/MusicBackendApplication.java
+++ Backend/MusicBackend/src/main/java/com/application/MusicBackendApplication.java
@@ -1,11 +1,15 @@
 package com.application;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableEncryptableProperties
 @EntityScan("com.application.*")
 public class MusicBackendApplication {
 
