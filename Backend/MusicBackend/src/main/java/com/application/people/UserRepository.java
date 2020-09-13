0a24400228e1ad697eeef450e139e5285e6645ff
Aidan <aidanwebster987@gmail.com>
2020-09-13 14:35:49 -0500

    Added search for users by email

--- Backend/MusicBackend/src/main/java/com/application/people/UserRepository.java
+++ Backend/MusicBackend/src/main/java/com/application/people/UserRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User,Long> {
     public User findByUsername(String username);
+    public User findByEmail(String email);
 }
