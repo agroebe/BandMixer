7851e5bea570986b9567902b61baa418d9553280
Aidan <aidanwebster987@gmail.com>
2020-09-14 15:49:54 -0500

    Made user creation checks more robust

--- Backend/MusicBackend/src/main/java/com/application/MainController.java
+++ Backend/MusicBackend/src/main/java/com/application/MainController.java
@@ -22,9 +22,18 @@ public class MainController {
         BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
-        if(userRepository.findByUsername(name) != null || userRepository.findByEmail(email) != null){
-            return "user already exists";
+
+        String nameAndEmailCheck = "";
+        if(userRepository.findByUsername(name) != null){
+            nameAndEmailCheck += "username is taken";
         }
+        if(userRepository.findByEmail(email) != null){
+            nameAndEmailCheck += "\nemail is already registered";
+        }
+        if(nameAndEmailCheck.length() > 0){
+            return nameAndEmailCheck;
+        }
+
         User n = new User();
         n.setEmail(email);
         n.setName(name);
