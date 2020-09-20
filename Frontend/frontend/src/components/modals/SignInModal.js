2de0ecaf8586bfdd11e01e095141724e9428af9f
Andrew Groebe <andy.groebe@gmail.com>
2020-09-20 12:47:48 -0500

    User sign in and other changes

--- Frontend/frontend/src/components/modals/SignInModal.js
+++ Frontend/frontend/src/components/modals/SignInModal.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
+import axios from 'axios';
+import { ToastContainer, toast, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './Modal.css'
 
 export default class SignInModal extends Component {
     constructor(props) {
         super(props);
-        console.log(props)
         this.state = {
-            username: '',
+            loginId: '',
             password: '',
+            responseExists: false,
+            response: '',
             show: false
         };
 
@@ -25,30 +29,60 @@ export default class SignInModal extends Component {
     }
 
     signIn() {
+        axios.post('http://coms-309-cy-01.cs.iastate.edu:8080/users/login?loginID=' + this.state.loginId + '&password=' + this.state.password).then(r => {
+            console.log(r.data)
+            this.setState({ response: r.data, responseExists: true })
+        })
+        
+        toast.success('Welcome to BandMixer, ' + this.state.loginId + '!', {
+            position: "top-center",
+            autoClose: 2500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            transition: Zoom
+        });
+
+        this.props.setLoggedIn(true)
+        this.props.setUserId(this.state.loginId)
+
         this.close();
     }
 
     render() {
         return(
-            <Modal size="lg" show={ this.state.show } onHide={ this.close }>
-                <Modal.Header>
-                    <Modal.Title>Sign In</Modal.Title>
-                </Modal.Header>
+            <>
+                <ToastContainer/>
+                <Modal size="lg" show={ this.state.show } onHide={ this.close }>
+                    <Modal.Header>
+                        <Modal.Title>Sign In</Modal.Title>
+                    </Modal.Header>
 
-                <Modal.Body>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" onChange={ e => this.setState({ username: e.target.value }) }/>
+                    <Modal.Body>
+                        <Form.Group>
+                            <Form.Label>Username or Email</Form.Label>
+                            <Form.Control type="text" onChange={ e => this.setState({ loginId: e.target.value }) }/>
+                        </Form.Group>
 
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" onChange={ e => this.setState({ password: e.target.value }) }/>
-                </Modal.Body>
+                        <Form.Group>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" onChange={ e => this.setState({ password: e.target.value }) }/>
+                        </Form.Group>
+                    </Modal.Body>
 
-                <Modal.Footer>
-                    <p className="redirect mr-auto" onClick={ this.props.openRegisterModal }>Don't have an account? Register here</p>
-                    <Button variant="success" onClick={ this.signIn }>Sign In</Button>
-                    <Button variant="danger" onClick={ this.close }>Cancel</Button>
-                </Modal.Footer>
-            </Modal>
+                    { this.state.responseExists &&
+                        <p className="text-danger">{ this.state.response }</p>
+                    }
+
+                    <Modal.Footer>
+                        <p className="redirect mr-auto" onClick={ this.props.openRegisterModal }>Don't have an account? Register here</p>
+                        <Button variant="success" onClick={ this.signIn }>Sign In</Button>
+                        <Button variant="danger" onClick={ this.close }>Cancel</Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
         )
     }
 }
