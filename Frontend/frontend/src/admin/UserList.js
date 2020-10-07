35be0d65e951933bd0f82f48576998137278ae05
Andrew Groebe <andy.groebe@gmail.com>
2020-10-07 16:14:43 -0500

    Cleanup

--- Frontend/frontend/src/admin/UserList.js
+++ Frontend/frontend/src/admin/UserList.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { List, Datagrid, TextField } from 'react-admin'
+import { List, Datagrid, TextField, EditButton, DeleteWithConfirmButton, CreateButton, TopToolbar } from 'react-admin'
 
 const UserList = (props) => {
-    return <List {...props}>
-        <Datagrid>
-            <TextField source='id'></TextField>
-        </Datagrid>
-    </List> 
+    return (
+        <>
+            <TopToolbar>Hi</TopToolbar>
+            <List {...props}>
+                <Datagrid>
+                    <TextField source='id'/>
+                    <TextField source='name'/>
+                    <TextField source='email'/>
+                    <TextField source='password'/>
+                    <EditButton/>
+                    <DeleteWithConfirmButton basePath="users/remove"/>
+                </Datagrid>
+            </List> 
+            <CreateButton/>
+        </>
+    )
 }
 
 export default UserList
