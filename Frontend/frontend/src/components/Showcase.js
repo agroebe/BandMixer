1a0d189bf29a44753147a2ac96ab76e1b3da05ec
Andrew Groebe <andy.groebe@gmail.com>
2020-10-14 12:40:59 -0500

    Start to frontend unit-testing

--- Frontend/frontend/src/components/Showcase.js
+++ Frontend/frontend/src/components/Showcase.js
@@ -61,9 +61,9 @@ export default class Showcase extends React.Component {
                     <p className="lead">Let's help you find your jam - start your search below!</p>
                     <Form inline className="justify-content-center mb-3" onSubmit={ this.search }>
                         <Form.Control as="select" className="mr-2" onChange={ e => this.setState({ instrument: e.target.value }) } >
-                            <option value={0} selected>Select what you're looking for</option>
+                            <option value={0}>Select what you're looking for</option>
                             { this.state.instrumentOptions.map((value, index) => {
-                                return <option value={ index + 1}>{value}</option>
+                                return <option value={ index + 1} key={index }>{value}</option>
                             }) }
                         </Form.Control>
                         <Form.Control type="text" placeholder="ZIP code or location" className="mr-2" onChange={ e => this.setState({ location: e.target.value }) }></Form.Control>
