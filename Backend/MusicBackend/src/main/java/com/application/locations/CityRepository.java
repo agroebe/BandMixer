5b7ae2149125b72c6f027381904fc2dfec122160
awebster <awebster@iastate.edu>
2020-10-28 23:27:53 +0000

    Delete CityRepository.java

--- Backend/MusicBackend/src/main/java/com/application/locations/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.locations;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends CrudRepository<City,Long> {
-    public City findByID(Long ID);
-}
