0affeedff30b41c83161913d28ee1a58f8b448ca
Andrew Groebe <andy.groebe@gmail.com>
2020-11-08 20:47:44 -0600

    Fixed footer, added some more post viewing features

--- Frontend/frontend/src/components/Footer.js
+++ Frontend/frontend/src/components/Footer.js
@@ -4,24 +4,12 @@ import { Row, Col } from 'react-bootstrap';
 export default class Footer extends React.Component {
     render() {
         return(
-            <footer className="text-center text-light bg-dark">
-                <Row>
-                    <Col>
-                        <strong><u>About</u></strong>
-                        <p>
-                            BandMixer is a platform used to connect musicians, offering a solution<br/>
-                            that they can use to create a band, join an existing one, or just find someone<br/>
-                            to jam with or learn from.
-                        </p>
-                    </Col>
-                    <Col>
-                        <strong><u>Context</u></strong>
-                        <p>
-                            For context, this website is created as a part of a semester-long project<br/>
-                            for the course CS 309 at Iowa State University. The CY_01 project team consists<br/>
-                            of Andrew Groebe, Timothy Schommer, Aidan Webster, and Aj Sandor</p>
-                    </Col>
-                </Row>
+            <footer className="text-center">
+                <p className="text-center mb-3">
+                    BandMixer is a platform used to connect musicians, offering a solution<br/>
+                    that they can use to create a band, join an existing one, or just find someone<br/>
+                    to jam with or learn from.
+                </p>
                 <p>Copyright &copy; BandMixer 2020</p>
             </footer>
         )
--- Frontend/frontend/src/routes/Post.js
+++ Frontend/frontend/src/routes/Post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios'
+import { Card, Image } from 'react-bootstrap'
 
 export default class Post extends React.Component {
     state = {
@@ -7,20 +8,29 @@ export default class Post extends React.Component {
     }
 
     componentDidMount() {// this.props.match.params.postId
-        axios({
-            method: 'get',
-            url: 'http://coms-309-cy-01.cs.iastate.edu:8080/posts/fetch',
-            data: {
-                post: {
-                    id: 144
-                }
-            }
+        axios.get('http://coms-309-cy-01.cs.iastate.edu:8080/posts/fetch/' + this.props.match.params.postId).then(r => {
+            this.setState({ post: r.data })
         })
     }
 
     render() {
         return(
-            <p>Hi</p>            
+            <>
+                { this.state.post ? (
+                    <Card style={{ width: '60rem', marginLeft: 'auto', marginRight: 'auto', marginBottom: '10px', marginTop: '10px' }}>
+                        <Card.Body>
+                        <Card.Title>{ this.state.post.title }</Card.Title>
+                        <Image style={{width: 100, height: 'auto', display: "inline-block"}} src="https://support.hubstaff.com/wp-content/uploads/2019/08/good-pic.png" roundedCircle />
+                        <Card.Subtitle className="mb-2 text-muted">Posted by { this.state.post.owner.username }</Card.Subtitle>
+                        <Card.Text>
+                            { this.state.post.textContent }
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                ) : (
+                    <p className="text-center">Post with id { this.props.match.params.postId } not found; try searching again!</p>
+                )}
+            </>
         )
     }
 }
