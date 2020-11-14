dca8c51ce765cba17f89c4d562c69a433f449cd7
Aidan <aidanwebster987@gmail.com>
2020-11-13 20:05:56 -0600

    Added chat endpoint websocket thing

---
+++ Backend/MusicBackend/src/main/java/com/application/chat/Message.java
@@ -0,0 +1,15 @@
+package com.application.chat;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
